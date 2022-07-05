d526affecc1b848a4cffc8a352cc85fb53b64085
Matt Jewell <m@mattjewell.co.uk>
2022-07-05 18:31:26 +0200

    added firebase auth

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,3 +1,4 @@
+// noinspection RequiredAttributes
 // In this file, all Page components from 'src/pages` are auto-imported. Nested
 // directories are supported, and should be uppercase. Each subdirectory will be
 // prepended onto the component name.
@@ -7,7 +8,7 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Set, Router, Route } from '@redwoodjs/router'
+import { Set, Router, Route, Private } from '@redwoodjs/router'
 
 import DifficultiesLayout from 'src/layouts/DifficultiesLayout'
 import ExercisePlansLayout from 'src/layouts/ExercisePlansLayout'
@@ -16,24 +17,27 @@ import ExercisesLayout from 'src/layouts/ExercisesLayout'
 const Routes = () => {
   return (
     <Router>
-      <Set wrap={DifficultiesLayout}>
-        <Route path="/difficulties/new" page={DifficultyNewDifficultyPage} name="newDifficulty" />
-        <Route path="/difficulties/{id:Int}/edit" page={DifficultyEditDifficultyPage} name="editDifficulty" />
-        <Route path="/difficulties/{id:Int}" page={DifficultyDifficultyPage} name="difficulty" />
-        <Route path="/difficulties" page={DifficultyDifficultiesPage} name="difficulties" />
-      </Set>
-      <Set wrap={ExercisesLayout}>
-        <Route path="/exercises/new" page={ExerciseNewExercisePage} name="newExercise" />
-        <Route path="/exercises/{id:Int}/edit" page={ExerciseEditExercisePage} name="editExercise" />
-        <Route path="/exercises/{id:Int}" page={ExerciseExercisePage} name="exercise" />
-        <Route path="/exercises" page={ExerciseExercisesPage} name="exercises" />
-      </Set>
-      <Set wrap={ExercisePlansLayout}>
-        <Route path="/exercise-plans/new" page={ExercisePlanNewExercisePlanPage} name="newExercisePlan" />
-        <Route path="/exercise-plans/{id:Int}/edit" page={ExercisePlanEditExercisePlanPage} name="editExercisePlan" />
-        <Route path="/exercise-plans/{id:Int}" page={ExercisePlanExercisePlanPage} name="exercisePlan" />
-        <Route path="/exercise-plans" page={ExercisePlanExercisePlansPage} name="exercisePlans" />
-      </Set>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Private unauthenticated="/login">
+        <Set wrap={DifficultiesLayout}>
+          <Route path="/difficulties/new" page={DifficultyNewDifficultyPage} name="newDifficulty" />
+          <Route path="/difficulties/{id:Int}/edit" page={DifficultyEditDifficultyPage} name="editDifficulty" />
+          <Route path="/difficulties/{id:Int}" page={DifficultyDifficultyPage} name="difficulty" />
+          <Route path="/difficulties" page={DifficultyDifficultiesPage} name="difficulties" />
+        </Set>
+        <Set wrap={ExercisesLayout}>
+          <Route path="/exercises/new" page={ExerciseNewExercisePage} name="newExercise" />
+          <Route path="/exercises/{id:Int}/edit" page={ExerciseEditExercisePage} name="editExercise" />
+          <Route path="/exercises/{id:Int}" page={ExerciseExercisePage} name="exercise" />
+          <Route path="/exercises" page={ExerciseExercisesPage} name="exercises" />
+        </Set>
+        <Set wrap={ExercisePlansLayout}>
+          <Route path="/exercise-plans/new" page={ExercisePlanNewExercisePlanPage} name="newExercisePlan" />
+          <Route path="/exercise-plans/{id:Int}/edit" page={ExercisePlanEditExercisePlanPage} name="editExercisePlan" />
+          <Route path="/exercise-plans/{id:Int}" page={ExercisePlanExercisePlanPage} name="exercisePlan" />
+          <Route path="/exercise-plans" page={ExercisePlanExercisePlansPage} name="exercisePlans" />
+        </Set>
+      </Private>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
---
+++ web/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react'
+
+import { useAuth } from '@redwoodjs/auth'
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const LoginPage = () => {
+  const { currentUser, isAuthenticated, logIn, logOut, getToken } = useAuth()
+
+  const [token, setToken] = useState(null)
+  useEffect(() => {
+    getToken()
+      .then((token) => setToken(token))
+      .catch((e) => {
+        setToken(null)
+        throw e
+      })
+  }, [setToken, currentUser])
+
+  return (
+    <>
+      <MetaTags title="Login" description="Login page" />
+
+      <h1>LoginPage</h1>
+      <p>
+        Find me in <code>./web/src/pages/LoginPage/LoginPage.tsx</code>
+      </p>
+      <p>
+        My default route is named <code>login</code>, link to me with `
+        <Link to={routes.login()}>Login</Link>`
+      </p>
+
+      <p>
+        {!isAuthenticated && <button onClick={() => logIn()}>Log In</button>}
+        {isAuthenticated && <button onClick={() => logOut()}>Log Out</button>}
+
+        {token && (
+          <textarea onClick={(e) => (e.target as HTMLTextAreaElement).select()}>
+            {JSON.stringify(
+              {
+                'auth-provider': 'firebase',
+                Authorization: `Bearer ${token}`,
+              },
+              null,
+              4
+            )}
+          </textarea>
+        )}
+        <code style={{ whiteSpace: 'pre' }}>
+          {JSON.stringify(currentUser, null, 4)}
+        </code>
+      </p>
+    </>
+  )
+}
+
+export default LoginPage
